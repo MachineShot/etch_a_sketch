d370416a363553ec64d0a782655fad081fdc69cb
machineshot <machineshot@outlook.com>
2019-07-18 18:57:03 +0300

    Updated

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 A browser version of something between a sketchpad and an Etch-A-Sketch.
 
 From The Odin Project's [curriculum](https://www.theodinproject.com/courses/web-development-101/lessons/etch-a-sketch-project?ref=lnav)
+
+Use your mouse to hover over pixels and paint them blue!
+
+Clear - reset the board
+
+Change size - change the amount of pixels per side
--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let container = document.getElementById("container");
 
-for(let i = 0; i < 16; i ++) { // create 16x16 div grid
-    for(let j = 0; j < 16; j++){
-        let div = document.createElement("div");
-        div.className = "blocks";
-        div.style.backgroundColor = "red";
-        container.appendChild(div);
+createGrid(16);
+
+function createGrid(num){
+    for(let i = 0; i < num; i ++) { // create 16x16 div grid
+        for(let j = 0; j < num; j++){
+            let div = document.createElement("div");
+            div.className = "blocks";
+
+            div.style.backgroundColor = "red";
+            div.style.height = 100/num + "%";
+            div.style.width = 100/num + "%";
+
+            container.appendChild(div);
+        }
     }
 }
 
@@ -15,7 +23,24 @@ function clearColor(){
     }
 }
 
-let changeColor = function(){
+function changeSize(){
+    let size = prompt('Enter size of pixels 2-64', 16);
+    
+    if(size > 1 && size < 65){
+        let list = document.querySelectorAll(".blocks")
+        list.forEach(el => container.removeChild(el));
+        createGrid(size);
+
+        for(let i = 0; i < blocks.length; i++){
+            blocks[i].addEventListener("mouseover", changeColor, false);
+        }
+    }
+    else{
+        changeSize();
+    }
+}
+
+function changeColor(){
     this.style.backgroundColor = "blue";
 }
 
